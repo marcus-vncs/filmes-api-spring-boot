108815389128d2bf3badca8090b70e5b5ecc8444
Marcus Vinicius <marcusvidigital@gmail.com>
2025-11-03 00:29:14 -0300

    update: Implementação do Patch

--- src/main/java/tech/ada/filmes_api/controller/FilmeController.java
+++ src/main/java/tech/ada/filmes_api/controller/FilmeController.java
@@ -57,21 +57,19 @@ public class FilmeController {
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         try {
             filmeService.deletarPorId(id);
-            // Retorna HTTP 204 No Content (Sucesso sem corpo de resposta)
             return ResponseEntity.noContent().build();
         } catch (Exception e) {
-            // Tratamento simples se houver falha na exclusão (ex: ID não existe)
             return ResponseEntity.notFound().build(); // HTTP 404 Not Found
         }
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Filme> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
-        // Por enquanto, retornamos o status de "Não Implementado" (501)
-        // apenas para garantir que o endpoint esteja mapeado.
-
-        // Retornaremos o status 501 (Not Implemented) temporariamente.
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
-                .build();
+    public ResponseEntity<Filme> atualizarParcial(@PathVariable Long id, @RequestBody Filme filmeDetalhes) {
+        try {
+            Filme filmeAtualizado = filmeService.atualizarParcial(id, filmeDetalhes);
+            return ResponseEntity.ok(filmeAtualizado);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/tech/ada/filmes_api/service/FilmeService.java
+++ src/main/java/tech/ada/filmes_api/service/FilmeService.java
@@ -1,7 +1,9 @@
 package tech.ada.filmes_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import tech.ada.filmes_api.model.Filme;
 import tech.ada.filmes_api.repository.FilmeRepository;
 
@@ -47,5 +49,30 @@ public class FilmeService {
             return filmeRepository.save(filmeExistente);
         }) .orElseThrow(() ->
                 new IllegalArgumentException("Filme não encontrado para o ID: " + id));
+    }
+
+    public Filme atualizarParcial(Long id, Filme filmeDetalhes) {
+
+        return filmeRepository.findById(id)
+                .map(filmeExistente -> {
+                    if (StringUtils.hasText(filmeDetalhes.getTitulo())) {
+                        filmeExistente.setTitulo(filmeDetalhes.getTitulo());
+                    }
+                    if (filmeDetalhes.getAnoLancamento() != null) {
+                        filmeExistente.setAnoLancamento(filmeDetalhes.getAnoLancamento());
+                    }
+                    if (StringUtils.hasText(filmeDetalhes.getDiretor())) {
+                        filmeExistente.setDiretor(filmeDetalhes.getDiretor());
+                    }
+                    if (StringUtils.hasText(filmeDetalhes.getGenero())) {
+                        filmeExistente.setGenero(filmeDetalhes.getGenero());
+                    }
+                    if (StringUtils.hasText(filmeDetalhes.getSinopse())) {
+                        filmeExistente.setSinopse(filmeDetalhes.getSinopse());
+                    }
+
+                    return filmeRepository.save(filmeExistente);
+                })
+                .orElseThrow(() -> new IllegalArgumentException("Filme não encontrado para o ID: " + id));
     }
 }
